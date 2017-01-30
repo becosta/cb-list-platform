638929da30d19a363fdbdb7406e453fcb4449796
Benjamin Costa <benjamin.costa.75@gmail.com>
2017-01-30 18:33:50 +0100

    Add various entities' forms

---
+++ src/CBList/ModelBundle/Form/ReportType.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017 Benjamin Costa <benjamin.costa.75@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace CBList\ModelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use CBList\ModelBundle\Entity\Report;
+
+
+/**
+ * Description of ReportType
+ *
+ * @author Benjamin Costa <benjamin.costa.75@gmail.com>
+ * @copyright (c) 2017, Benjamin Costa
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+class ReportType extends AbstractType
+{
+
+    const ENTITY_NAME       = 'report';
+    const HOST_FIELD        = 'host';
+    const CATEGORY_FIELD    = 'category';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(self::HOST_FIELD)->add(self::CATEGORY_FIELD);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+                array(
+                    'data_class' => 'CBList\ModelBundle\Entity\Report',
+                    'csrf_protection' => false,
+                )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::ENTITY_NAME;
+    }
+}
